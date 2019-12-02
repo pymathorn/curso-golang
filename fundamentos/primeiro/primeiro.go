65d0ef820f6a10237462a073f9e6cb90c244e6e0
pymathorn <57976514+pymathorn@users.noreply.github.com>
2019-12-02 15:18:42 -0300

    nova pasta: fundamentos

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,12 @@
+// Programas executáveis iniciam pelo pacote main
+package main
+/*
+Os códigos em go são organizados em pacotes
+e para usá-los é preciso usar um ou vários impots
+*/
+import "fmt"
+//A porta de entrado de um programa é a função main
+func main() {
+	fmt.Print("Primeiro \n")
+	fmt.Print("Programa!!! \n")
+}
